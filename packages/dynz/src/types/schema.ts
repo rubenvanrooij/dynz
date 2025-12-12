3fe62375bb2a24f218129fe75013f1aab3c87fe3
Ruben van Rooij <ruben.van.rooij@gmail.com>
2025-12-12 09:51:08 +0100

    fix: fixed issue with options type inference

--- packages/dynz/src/types/schema.ts
+++ packages/dynz/src/types/schema.ts
@@ -138,7 +138,7 @@ export type SchemaValuesInternal<T extends Schema> = T extends ObjectSchema<neve
     : T extends EnumSchema
       ? MakeOptional<T, EnumValues<T["enum"]>>
       : T extends OptionsSchema
-        ? T["options"]
+        ? Unpacked<T["options"]>
         : MakeOptional<T, ValueType<T["type"]>>;
 
 export type SchemaValues<T extends Schema> = Prettify<ApplyPrivacyMask<T, SchemaValuesInternal<T>>>;
