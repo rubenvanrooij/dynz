825be9e8e928de0ddf765210597a0485e823794c
Ruben v. Rooij <ruben.van.rooij@gmail.com>
2026-04-15 14:26:47 +0200

    fix: fixed some examples

--- examples/next-example/src/app/[locale]/dependent-form/page.tsx
+++ examples/next-example/src/app/[locale]/dependent-form/page.tsx
@@ -4,7 +4,7 @@ import { eq, object, ref, type Schema, type StrFluent, string } from "dynz";
 import { DynzForm } from "@/components/dynz/form";
 import { DynzInput } from "@/components/dynz/input";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const FIELD_COUNT = 10;
 
@@ -20,7 +20,7 @@ const schema = object(
     }
 
     if (index > 1) {
-      fieldSchema = fieldSchema.setIncluded(eq(ref(`field_${index - 1}`), "aa"));
+      fieldSchema = fieldSchema.setIncluded(eq(ref(`field_${index - 1}`), "hello"));
     }
 
     prev[`field_${index}`] = fieldSchema;
@@ -31,9 +31,13 @@ const schema = object(
 export default function Page() {
   return (
     <Card className="flex flex-col gap-4 max-w-100">
+      <CardHeader>
+        <CardTitle>Start type 'hello' in every input field to see dynamic inclusion</CardTitle>
+      </CardHeader>
       <CardContent className="gap-2">
         <DynzForm name="loginForm" schema={schema}>
           {Array.apply(null, Array(FIELD_COUNT)).map((_, index) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: demo purpose
             <DynzInput key={`field_${index}`} name={`field_${index}`} type="text" />
           ))}
 
