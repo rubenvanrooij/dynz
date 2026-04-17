405631b4eac98098d984c3589744ba340cfa697b
Ruben v. Rooij <ruben.van.rooij@gmail.com>
2026-04-17 09:54:30 +0200

    chore: fixed formatting issues

--- examples/example/src/01-basic-schema.ts
+++ examples/example/src/01-basic-schema.ts
@@ -1,12 +1,11 @@
 import * as d from "dynz";
 
 async function runExample() {
+  const stringSchema = d.string().min(1).max(10).optional();
 
-    const stringSchema = d.string().min(1).max(10).optional()
+  const result = await d.validate(stringSchema, undefined, "122");
 
-    const result = await d.validate(stringSchema, undefined, '122');
-
-    console.log("result: ", result);
+  console.log("result: ", result);
 }
 
-runExample()
+runExample();
--- examples/example/src/03-conditional-field-visibility.ts
+++ examples/example/src/03-conditional-field-visibility.ts
@@ -1,27 +1,28 @@
 import * as d from "dynz";
 
 async function runExample() {
+  const schema = d.object({
+    status: d.options(["employed", "self-employed", "unemployed"]),
+    // Only included when employed
+    employerName: d
+      .string()
+      .min(2)
+      .setIncluded(d.eq(d.ref("status"), "employed")),
 
-    const schema = d.object({
-        status: d.options(["employed", "self-employed", "unemployed"]),
-        // Only included when employed
-        employerName: d.string()
-            .min(2)
-            .setIncluded(d.eq(d.ref("status"), "employed")),
+    // Only included when self-employed
+    businessName: d
+      .string()
+      .min(2)
+      .setIncluded(d.eq(d.ref("status"), "self-employed")),
+  });
 
-        // Only included when self-employed
-        businessName: d.string()
-            .min(2)
-            .setIncluded(d.eq(d.ref("status"), "self-employed")),
-    });
+  // Try changing confirmPassword to not match!
+  const result = await d.validate(schema, undefined, {
+    status: "employed",
+    employerName: "secretpass123",
+  });
 
-    // Try changing confirmPassword to not match!
-    const result = await d.validate(schema, undefined, {
-        status: "employed",
-        employerName: "secretpass123",
-    });
-
-    console.log("result: ", result);
+  console.log("result: ", result);
 }
 
-runExample()
+runExample();
